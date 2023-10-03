950a6face67d5ff9eff8313353c133ce9cba7de3
unmandas <unmandas1998@gmail.com>
2023-10-03 18:41:49 +0530

    Conversation UI added

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,4 @@
 import Navbar from '@/components/navbar'
-import React from 'react'
 import Sidebar from '@/components/sidebar'
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import { Montserrat } from "next/font/google"
 
 import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 import {
     LayoutDashboard,
     MessageSquare,
@@ -63,10 +64,12 @@ const routes = [
 ]
 
 const Sidebar = () => {
+    const pathname = usePathname();
+
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white ">
             <div className="px-3 py-2 flex-1">
-                <Link href="/" className="flex intems-center pl-3 mb-14">
+                <Link href="/" className="flex items-center pl-3 mb-14">
                     <div className="relative w-8 h-8 mr-4">
                         <Image
                             fill
@@ -83,15 +86,14 @@ const Sidebar = () => {
                         <Link
                             href={route.href}
                             key={route.href}
-                            className="text-sm group flex p-3 justify-start front-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+                            className={cn("text-sm group flex p-3 justify-start front-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}
                         >
                             <div className="flex items-center flex-1">
                                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
                                 {route.label}
                             </div>
                         </Link>
-                    ))
-                    }
+                    ))}
                 </div>
             </div>
         </div>
